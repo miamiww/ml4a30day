3a7e1a0d09c27eda904356cf0b5d8c59706396de
miamiworldwide <rivendalejones@gmail.com>
2018-07-15 15:56:27 +0900

    adding some better dialogues

--- projects/face_recognize/osc_listener.js
+++ projects/face_recognize/osc_listener.js
@@ -4,7 +4,7 @@ var faceWatch = true;
 
 var udpPort = new osc.UDPPort({
 	localAddress: "localhost",
-	localPort: 12000 
+	localPort: 12000
 });
 
 udpPort.on("ready", function () {
@@ -20,14 +20,27 @@ udpPort.on("message", function(oscMessage){
 //	console.log(oscMessage);
 	if(oscMessage.args[0] > 0.8 && faceWatch){
 		beep();
-		console.log('welcome alden');
+		console.log('welcome alden :)');
 		faceWatch = false;
 	} else {
-		if(oscMessage.args[0] < 0.8){
+		if(oscMessage.args[0] < 0.6){
 		faceWatch = true;
+		setTimeout(whereAre,1000);
+		setTimeout(missYou,1500);
+		setTimeout(noHide,2000);
 		}
 	}
 });
+
+function whereAre(){
+		console.log('where are you alden');
+}
+function missYou(){
+		console.log('I miss you alden');
+}
+function noHide(){
+		console.log('plz no more hide.');
+}
 
 udpPort.on("error", function(err){
 	console.log(err);
